c345d6e8a4540f42a1b0dfab39f264b892cd4656
Njoku Emmanuel <kalunjoku123@gmail.com>
2019-08-26 18:47:17 +0100

    added react navigation

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StyleSheet, Text, View } from "react-native";
+import { createSwitchNavigator, createStackNavigator } from 'react-navigation';
 import * as firebase from "firebase";
 import React from "react";
 import ApiKeys from "./components/constants/ApiKeys";
-import 
+import screenChoice from "./components/main/screenChoice"
 
  export default class App extends React.Component {
   constructor(props) {
