3b90041f63919add91b310adc6b3d2e486f9c306
Njoku Emmanuel <kalunjoku123@gmail.com>
2019-08-26 15:55:54 +0100

    created login and sign up file

---
+++ screens/customer/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { View } from "native-base";
+export class home extends Component {
+  render() {
+    return (
+      <View>
+        <Text></Text>
+      </View>
+    );
+  }
+}
+
+export default home;
---
+++ screens/supplier/login.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { View } from "native-base";
+
+export class login extends Component {
+  render() {
+    return (
+      <View>
+        <Text></Text>
+      </View>
+    );
+  }
+}
+
+export default login;
