2284bbdb2bf985289ab2f175fa66af972b32cae9
Njoku Emmanuel <kalunjoku123@gmail.com>
2019-08-30 18:10:47 +0100

    solved api problem

--- components/constants/ApiKey.js
+++ components/constants/ApiKey.js
@@ -1,11 +1,11 @@
-export default {
-  firebaseConfig: {
-    apiKey: "AIzaSyARr9mhXss5vqr1ZWzyfWw6sI6rNeNWxBQ",
-    authDomain: "q-aqua.firebaseapp.com",
-    databaseURL: "https://q-aqua.firebaseio.com",
-    projectId: "q-aqua",
-    storageBucket: "",
-    messagingSenderId: "154780930991",
-    appId: "1:154780930991:web:76cb93f7cf55b812"
-  }
+import * as firebase from "firebase";
+var config = {
+  apiKey: "AIzaSyARr9mhXss5vqr1ZWzyfWw6sI6rNeNWxBQ",
+  authDomain: "q-aqua.firebaseapp.com",
+  databaseURL: "https://q-aqua.firebaseio.com",
+  projectId: "q-aqua",
+  storageBucket: "",
+  messagingSenderId: "154780930991",
+  appId: "1:154780930991:web:76cb93f7cf55b812"
 };
+firebase.initializeApp(config);
