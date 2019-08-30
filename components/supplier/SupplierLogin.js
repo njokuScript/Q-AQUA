7033b9e8b7d1c1e76d47bc6762a0391488570b68
Njoku Emmanuel <kalunjoku123@gmail.com>
2019-08-30 17:27:29 +0100

    made reset password functional

--- components/supplier/SupplierLogin.js
+++ components/supplier/SupplierLogin.js
@@ -60,7 +60,7 @@ export default class SupplierLogin extends React.Component {
           <TouchableOpacity style={styles.button} onPress={this.signInAsync}>
             <Text style={styles.buttonText}>LOGIN</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.reset}>
+          <TouchableOpacity style={styles.reset} onPress={this._resetPassWord}>
             <Text style={styles.buttonTextRes}>RESET PASSWORD</Text>
           </TouchableOpacity>
           <View style={styles.signupContent}>
